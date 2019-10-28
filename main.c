f6bdd7fd52b5d6b78dac7c0e8355bb8c6b9a035b
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2019-10-28 05:57:02 +0530

    coded

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+//Even odd upto n
+#include<stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int i = 1, j, k;
+    printf("Enter the  no. upto which you want to see EVEN and ODD no.");
+    scanf("%d", &k);
+    a:
+    if (i <= k && i % 2 == 0) {
+        printf("%d is EVEN\n", i);
+        i = i + 1;
+        goto a;
+    } else if (i <= k && i % 2 != 0) {
+        printf("%d is ODD\n", i);
+        i = i + 1;
+        goto a;
+    }
 }
